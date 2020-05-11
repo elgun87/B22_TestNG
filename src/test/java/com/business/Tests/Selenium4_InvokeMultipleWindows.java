39e2f2cc213b77c06bed5b08b1e8078ce0ab6858
Salmanzade <salmanovanar@gmail.com>
2020-05-11 03:28:19 -0500

    Selenium 4 new Functions

---
+++ src/test/java/com/business/Tests/Selenium4_InvokeMultipleWindows.java
@@ -0,0 +1,55 @@
+package com.business.Tests;
+
+import com.business.Utilities.Base;
+import com.business.Utilities.JSUtil;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.withTagName;
+
+public class Selenium4_InvokeMultipleWindows extends Base {
+
+    /*
+    Scenario ; Go to https://rahulshettyacademy.com/angularpractice/"
+    in name field add 1st course name from  https://rahulshettyacademy.com
+     */
+    @Test
+    public void invokeMultipleWindows() throws InterruptedException, IOException {
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        WebElement editbox = driver.findElement(By.xpath("//div[@class='form-group']//input[@name='name']"));
+        // Open new URL in another Tab
+        driver.switchTo().newWindow(WindowType.TAB);
+        Thread.sleep(1000);
+        // Driver still points to 1st window, handle all open windows and switch to next window
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> it = windows.iterator();
+        String parentWindow = it.next(); // points current parent window
+        String childWindow = it.next(); // must be declared after parent window, will point next child window
+        driver.switchTo().window(childWindow);
+        driver.get("https://rahulshettyacademy.com");
+        List<WebElement> courses = driver.findElements(By.xpath("//h2//a"));
+        String courseName = courses.get(0).getText();
+        driver.switchTo().window(parentWindow);
+        editbox.sendKeys(courseName);
+        Thread.sleep(2000);
+        // Taking Screenshot of WebElement
+        File file = editbox.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("editbox.png"));
+        // Get Height and Width of element
+        int height = editbox.getRect().getDimension().getHeight();
+        int width = editbox.getRect().getDimension().getWidth();
+        System.out.println("height : " + height + ", width : " + width);
+
+    }
+
+}
---
+++ src/test/java/com/business/Tests/Selenium4_RelativeLocators.java
@@ -0,0 +1,37 @@
+package com.business.Tests;
+
+import com.business.Utilities.Base;
+import com.business.Utilities.JSUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.withTagName;
+
+public class Selenium4_RelativeLocators extends Base {
+
+    @Test
+    public void testingSelenium4RelativeLocators() {
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        // withTagName("tag").below(element);
+        WebElement nameLabel = driver.findElement(By.xpath("//label[text()='Name']"));
+        WebElement nameEditBox = driver.findElement(withTagName("input").below(nameLabel));
+        nameEditBox.sendKeys("Hi World");
+        // withTagName("tag").above(element);
+        WebElement birthEditbox = driver.findElement(By.xpath("//div[@class='form-group']//input[@name='bday']"));
+        WebElement birthText = driver.findElement(withTagName("label").above(birthEditbox));
+        System.out.println("text " + birthText.getText());
+        // withTagName("tag").toRightOf(element);
+        WebElement checkBox = driver.findElement(By.id("exampleCheck1"));
+        WebElement nameOfCheckBox = driver.findElement(withTagName("label").toRightOf(checkBox));
+        System.out.println(nameOfCheckBox.getText());
+        // withTagName("tag").toLeftOf(element);
+        WebElement statusChecbox = driver.findElement(By.id("inlineRadio1"));
+        WebElement checxBoxText = driver.findElement(withTagName("label").toLeftOf(statusChecbox));
+        System.out.println(checxBoxText.getText());
+
+
+
+    }
+
+}
